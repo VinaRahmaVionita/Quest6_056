12ec6ffdf2f16679115f89b1e01e125303ad3bf9
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-22 14:18:25 +0700

    membuat variable private _mahasiswaStateUi

---
+++ app/src/main/java/com/example/project6/ui/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.project6.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.project6.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MahasiswaViewModel : ViewModel() {
+
+    private val _mahasiswaStateUI =
+        MutableStateFlow(Mahasiswa())
+    //request atau state
+
+}
