5b5fdc5ac0ccf6e14f85d938a7c5c209a5a65631
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-22 13:44:10 +0700

    membuat halaman matakuliah ketika diklik akan lanjut ke halaman tampil

--- app/src/main/java/com/example/project6/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/project6/navigation/PengelolaHalaman.kt
@@ -67,6 +67,16 @@ fun MahasiswaApp(
             )
         }
 
+        composable(route = Halaman.Matakuliah.name){
+            RencanaStudyView(
+                mahasiswa = mahasiswaUiState,
+                onSubmitButtonClicked = {
+                    krsViewModel.saveDataKRS(it)
+                    navController.navigate(Halaman.Tampil.name) },
+                onBackButtonClicked = { navController.popBackStack() }
+            )
+        }
+
 
     }
 }
