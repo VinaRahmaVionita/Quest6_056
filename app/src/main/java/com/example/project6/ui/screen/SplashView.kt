8ffcff15e4ad51ef0edd1b5ccb186b7d3ac6d43f
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-22 14:04:49 +0700

    membuat fungsi splash view dan parameter utamanya

---
+++ app/src/main/java/com/example/project6/ui/screen/SplashView.kt
@@ -0,0 +1,29 @@
+package com.example.project6.ui.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.project6.R
+
+
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit
+) {
+
+    }
+}
