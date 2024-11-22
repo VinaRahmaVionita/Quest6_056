ad664643c5d2c547065f5278d716ce24dcee4728
vinarahmavionita <vinarahmavionita824@gmail.com>
2024-11-22 13:37:52 +0700

    membuat enum class halaman (apa saja screen yang akan di tampilkan)

---
+++ app/src/main/java/com/example/project6/navigation/PengelolaHalaman.kt
@@ -0,0 +1,32 @@
+package com.example.project6.navigation
+
+import android.service.controls.Control
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.project6.model.Mahasiswa
+import com.example.project6.ui.screen.MahasiswaFormView
+import com.example.project6.ui.screen.RencanaStudyView
+import com.example.project6.ui.screen.SplashView
+import com.example.project6.ui.screen.TampilView
+import com.example.project6.ui.viewmodel.MahasiswaViewModel
+import com.example.project6.ui.viewmodel.RencanaStudyViewModel
+import com.example.project6.ui.widget.DynamicSelectTextField
+
+enum class Halaman {
+    Splash,
+    Mahasiswa,
+    Matakuliah,
+    Tampil
+}
+
